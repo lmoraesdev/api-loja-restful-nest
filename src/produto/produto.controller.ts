f482e4f96ef3edd42a0d32e0eaedc5daff39947c
lmoraesdev <lbc_moraes@outlook.com>
2023-08-14 18:16:40 -0300

    feat: dto

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Body, Post, Get } from '@nestjs/common';
 import { ProdutoRepository } from './produto.repository';
+import { CriaProdutoDTO } from './dto/CriaProduto.dto';
 
 @Controller('/produtos')
 export class ProdutoController {
   constructor(private produtoRepository: ProdutoRepository) {}
 
   @Post()
-  async criaProduto(@Body() dadosDoProduto) {
+  async criaProduto(@Body() dadosDoProduto: CriaProdutoDTO) {
     this.produtoRepository.salvar(dadosDoProduto);
     return dadosDoProduto;
   }
