fa38a232d535c0f573a75924909ea207ff4596f1
lmoraesdev <lbc_moraes@outlook.com>
2023-08-21 14:07:13 -0300

    feat: crud

--- src/produto/produto.repository.ts
+++ src/produto/produto.repository.ts
@@ -1,14 +1,45 @@
+import { ProdutoEntity } from './produto.entity';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProdutoRepository {
-  private produtos = [];
+  private produtos: ProdutoEntity[] = [];
 
-  async salvar(produto) {
-    this.produtos.push(produto);
+  listaTodos() {
+    return this.produtos;
   }
 
-  async listar() {
-    return this.produtos;
+  salva(dadosProduto: ProdutoEntity) {
+    this.produtos.push(dadosProduto);
+    return dadosProduto;
+  }
+
+  private buscaPorId(id: string) {
+    const possivelProduto = this.produtos.find((produto) => produto.id === id);
+
+    if (!possivelProduto) {
+      throw new Error('Produto não existe');
+    }
+
+    return possivelProduto;
+  }
+
+  async atualiza(id: string, dadosProduto: Partial<ProdutoEntity>) {
+    const dadosNaoAtualizaveis = ['id', 'usuarioId'];
+    const produto = this.buscaPorId(id);
+    Object.entries(dadosProduto).forEach(([chave, valor]) => {
+      if (dadosNaoAtualizaveis.includes(chave)) {
+        return;
+      }
+      produto[chave] = valor;
+    });
+
+    return produto;
+  }
+
+  async remove(id: string) {
+    const produtoRemovido = this.buscaPorId(id);
+    this.produtos = this.produtos.filter((produto) => produto.id !== id);
+    return produtoRemovido;
   }
 }
